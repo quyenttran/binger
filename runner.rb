c798a0a1883784e1ca8d9200aded101451ebf5df
Paul Newsam <email": null>
2017-05-25 14:15:41 -0700

    LyricsGetter class with HTTP gets

--- runner.rb
+++ runner.rb
@@ -6,27 +6,24 @@ class LyricsGetter
 
   include HTTParty
   base_uri 'https://api.musixmatch.com/ws/1.1'
-  # default_params =
+  default_params(format: 'json', callback: 'callback')
 
-  def initialize(track, artist, key)
-    @options = { query: {
-      format: 'json',
-      callback: 'callback',
-      q_track: track,
-      q_artist: artist,
-      apikey: key } }
+  def initialize(key)
+    self.class.default_params(apikey:key)
   end
 
-  def get_lyrics
-    self.class.get("/matcher.lyrics.get", @options)
+  def get_lyrics(options = {})
+    self.class.get("/matcher.lyrics.get", options)
   end
 
 end
 
 key = ENV['MUSIXMATCHER_API_KEY']
 
-# response = HTTParty.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=roxanne&q_artist=sting' + "&apikey=#{key}")
+# response = HTTParty.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=roxanne&callback=callback&q_artist=sting&format=jsonp' + "&apikey=#{key}")
 
-roxanne = LyricsGetter.new('roxanne', 'sting', key)
-roxanne_lyrics = JSON.parse(roxanne.get_lyrics)
-puts roxanne_lyrics['message']['body']['lyrics']['lyrics_body']
+tvz = { query: { q_track: 'colorado-girl', q_artist: 'townes-van-zandt' } }
+
+lyricsgetter = LyricsGetter.new(key)
+coloradogirl = JSON.parse(lyricsgetter.get_lyrics(tvz))
+puts coloradogirl['message']['body']['lyrics']['lyrics_body']
